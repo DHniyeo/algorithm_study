8cfe080e34867a5a8b75838930b15e0df844ffd2
DHniyeo <64246124+DHniyeo@users.noreply.github.com>
2023-09-18 00:33:27 +0900

    [Gold V] Title: 강의실 배정, Time: 84 ms, Memory: 4316 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.cc
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.cc
@@ -1,29 +1,48 @@
-#include<iostream>
-#include<queue>
-#include<algorithm>
+#include <stdio.h>
+#include <queue>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
+struct info {
+	int start;
+	int end;
+};
+
+bool cmp(const info & a, const info &b) {
+	if (a.start < b.start) {
+		return true;
+	}
+	else if (a.start == b.end) {
+		if (a.end < b.end) {
+			return true;
+		}
+	}
+	return false;
+}
+
 int main() {
-	int n, tmp1, tmp2;
-	vector<pair<int, int>> st;
-	priority_queue<int, vector<int>, greater<int>> t;
+	int n;
+	scanf("%d", &n);
+	
+	vector<info> v;
+	priority_queue<int, vector<int>, greater<int>> endtime;
 
-	cin >> n;
-
-	while (n--) {
-		cin >> tmp1 >> tmp2;
-		st.push_back(make_pair(tmp1, tmp2));
+	for (int i = 0; i < n; i++) {
+		info tmp;
+		scanf("%d %d", &tmp.start, &tmp.end);
+		v.push_back(tmp);
+	}
+	// 기본적으로 첫번째 원소 기준 정렬임.
+	sort(v.begin(), v.end(), cmp);
+	
+	for (int i = 0; i < v.size(); i++) {
+		endtime.push(v[i].end);
+		if (endtime.top() <= v[i].start) endtime.pop();
 	}
 
-	sort(st.begin(), st.end());
-
-	for (int i = 0; i < st.size(); i++) {
-		t.push(st[i].second);
-   		if (t.top() <= st[i].first) t. pop();
-	}
-
-	cout << t.size();
+	printf("%d", endtime.size());
 
 
-	return 0;
 }
