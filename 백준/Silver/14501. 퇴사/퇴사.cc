4ee0dfe38bda9659d37718ec358dc8338bb187e8
DHniyeo <64246124+DHniyeo@users.noreply.github.com>
2024-08-20 21:38:23 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -11,13 +11,12 @@ int DP(int n) {
 
 	int max = 0;
 	// DP 테이블 생성
-	for (int i = 1; i <= n; i++) {
+	for (int i = 1; i <= n+1; i++) {
 		max = memo[i] > max ? memo[i] : max;
 		if (arr[i].T + i > n + 1) continue;
 		int tmp = max + arr[i].P; // 현재 최고 금액 + 해당 상담 후 받을 금액
 		memo[arr[i].T + i] = memo[arr[i].T + i] < tmp ? tmp : memo[arr[i].T + i];
 	}
-	max = memo[n + 1] > max ? memo[n + 1] : max;
 	return max;
 }
 int main()
