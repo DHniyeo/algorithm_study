821e4bac39d577ec2de253b03c9bac526d8fefc8
DHniyeo <64246124+DHniyeo@users.noreply.github.com>
2024-11-30 22:25:56 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 644 ms, Memory: 8180 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,32 +1,61 @@
 #include<iostream>
-#include<unordered_map>
 #include<vector>
+#include<algorithm>
 using namespace std;
+
+vector<int> dict;
+vector<int> target;
 int N, M;
-unordered_map<int,int> dict;
-vector<int> vc;
-int st = -1e9;
-int en = 1e9;
 void init() {
 	cin >> N;
 	for (int i = 0; i < N; i++) {
 		int num;
 		cin >> num;
-		if(dict[num]) dict[num] += 1;
-		else dict[num] = 1;
+		dict.push_back(num);
 	}
 	cin >> M;
 	for (int i = 0; i < M; i++) {
 		int num;
 		cin >> num;
-		vc.push_back(num);
+		target.push_back(num);
 	}
+	sort(dict.begin(), dict.end());
 }
-
+int upper_bound(int find_val) {
+	int st = 0;
+	int en = N;
+	while (st < en) {
+		int mid = (st + en) / 2;
+		if (dict[mid] > find_val) {
+			en = mid;
+		}
+		else {
+			st = mid + 1;
+		}
+	}
+	return st;
+}
+int lower_bound(int find_val){
+	int st = 0;
+	int en = N;
+	while (st < en) {
+		int mid = (st + en) / 2;
+		if (dict[mid] >= find_val) {
+			en = mid;
+		}
+		else {
+			st = mid + 1;
+		}
+	}
+	return st;
+}
 
 int main() {
 	init();
-	for (auto num : vc) {
-		cout << dict[num] << " ";
+
+	for (auto a : target) {
+		// cout << upper_bound(dict.begin(), dict.end(), a) - lower_bound(dict.begin(), dict.end(), a) << " ";
+		cout << upper_bound(a) - lower_bound(a) << " ";
 	}
+	
 }
